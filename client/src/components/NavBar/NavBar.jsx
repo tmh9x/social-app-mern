78b1d3480c10d20aaebc41bb521b4c5505067bab
tmh9x <thomas.hermanu@gmail.com>
2022-08-16 10:32:38 +0200

    add navbar component

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="navbar-container">
+      <h2> social-app</h2>
+    </div>
+  );
+}

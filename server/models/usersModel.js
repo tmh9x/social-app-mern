cd6bde5ad1e4824b341910c475608edb9de3f402
tmh9x <thomas.hermanu@gmail.com>
2022-09-08 16:08:02 +0200

    export usersSchema

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const usersSchema = new mongoose.Schema({
+export const usersSchema = new mongoose.Schema({
   userName: {
     type: String,
     required: false,
